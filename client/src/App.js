519d3ed2dc193b99a7171af58e5b001a27c7a7ed
Kevin Flynn <kevin@192.168.1.10>
2023-01-03 14:32:53 -0600

    Added search bar compontnet and title

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import SearchBar from './SearchBar';
 
 function App() {
   return (
-    <div>
-      Hello World!
+    <div class="main-header">
+      <h1>Movie Review Hub</h1>
+        <SearchBar />
     </div>
   );
 }
